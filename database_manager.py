86b477bca0e0ffff3c371784f8933ffaad236908
SadDiamonds <themostevilsans@gmail.com>
2025-08-25 11:20:08 +1000

    slig

---
+++ database_manager.py
@@ -0,0 +1,10 @@
+import sqlite3 as sql
+
+
+def listExtension():
+    con = sql.connect("database/data_source.db")
+    cur = con.cursor()
+    data = cur.execute("SELECT * FROM donations").fetchall()
+    
+    con.close()
+    return data
